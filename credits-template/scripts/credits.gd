d6c3ec1a628f4c67e16e47e9d40d48a85b12f932
Falme Streamless <falme_streamless@hotmail.com>
2026-01-11 05:50:26 -0300

    some code refactoring

--- credits-template/scripts/credits.gd
+++ credits-template/scripts/credits.gd
@@ -2,12 +2,14 @@
 class_name Credits
 extends VBoxContainer
 
+signal credits_finished
+
+@export var velocity : float
+var _isScrolling : bool = false
+
+# Editor Buttons for Debug
 @export_tool_button("Start Credits", "Callable") var roll_credits_action : Callable = start_credits
 @export_tool_button("Stop Credits", "Callable") var stop_credits_action : Callable = stop_credits
-
-var _isScrolling : bool = false
-@export var velocity : float
-@export var end_node : Control
 
 func start_credits():
 	scroll_to_start()
@@ -16,28 +18,33 @@ func start_credits():
 func stop_credits():
 	stop_scrolling()
 
-
+# Script methods
 func _ready() -> void:
+	$End.end_reached.connect(credits_ended)
+
 	scroll_to_start()
 	start_scrolling()
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if(_isScrolling):
-		scroll(_delta)
-		if(end_node.global_position.y < 0):
-			_isScrolling = false
+		scroll(delta)
 
-func scroll_at(_y : float) -> void:
+func scroll_to(_y : float) -> void:
 	self.position.y = _y
 
 func scroll_to_start() -> void:
 	self.position.y = DisplayServer.screen_get_size().y
 
 func scroll(delta: float) -> void:
-	scroll_at(self.position.y + (-velocity * delta))
+	self.position.y += (-velocity * delta)
 
 func start_scrolling() -> void:
 	_isScrolling = true
 
 func stop_scrolling() -> void:
 	_isScrolling = false
+
+func credits_ended(offset : float) -> void:
+	stop_scrolling()
+	scroll_to(self.position.y - offset)
+	credits_finished.emit()
