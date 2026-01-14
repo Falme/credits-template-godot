20bf14d7ba50352938e846711aa7e6a8ef4ffa58
Falme Streamless <falme_streamless@hotmail.com>
2026-01-14 08:05:17 -0300

    credits load labels for category and actors

--- credits-template/scripts/credits_staff.gd
+++ credits-template/scripts/credits_staff.gd
@@ -2,11 +2,31 @@ class_name CreditsStaff
 extends VBoxContainer
 
 @export var label_title : PackedScene
+@export var label_category : PackedScene
+@export var label_actor : PackedScene
 
 func load_data(data : Dictionary) -> void:
 	write_title(data.title)
+	write_staff(data.labels)
 
 func write_title(title : String) -> void:
 	var instance = label_title.instantiate()
 	instance.text = title
+	add_child(instance)
+
+func write_staff(staff : Array) -> void:
+	for category in staff:
+		write_category(category[0])
+		for actor in range(1, category.size()):
+			write_actor(category[actor])
+
+
+func write_category(category : String) -> void:
+	var instance = label_category.instantiate()
+	instance.text = category 
+	add_child(instance)
+
+func write_actor(actor : String) -> void:
+	var instance = label_actor.instantiate()
+	instance.text = actor 
 	add_child(instance)
